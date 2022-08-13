d8f107b9e8f3de9229c610d01403dd5ffd22dab9
krhmwn <krhmwn@users.noreply.github.com>
2022-08-13 13:20:23 +0700

    Update eleventy.js 13.08.22

--- .eleventy.js
+++ .eleventy.js
@@ -17,7 +17,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginNavigation);
 
   eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LL yyyy");
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd/LL/yyyy");
   });
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
