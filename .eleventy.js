4752267ebeabbb1b7f81088ee59090f9e4f4aa8f
krhmwn <krhmwn@users.noreply.github.com>
2022-09-16 12:51:33 +0700

    Update eleventy.js 16.09.22 5

--- .eleventy.js
+++ .eleventy.js
@@ -9,21 +9,12 @@ const _ = require("lodash");
 
 module.exports = function(eleventyConfig) {
   
-config.addCollection("postsByYear", (collection) => {
-  const posts = collection.getFilteredByTag('post').reverse();
-  const years = posts.map(post => post.date.getFullYear());
-  const uniqueYears = [...new Set(years)];
-
-  const postsByYear = uniqueYears.reduce((prev, year) => {
-    const filteredPosts = posts.filter(post => post.date.getFullYear() === year);
-
-    return [
-      ...prev,
-      [year, filteredPosts]
-    ]
-  }, []);
-
-  return postsByYear;
+eleventyConfig.addCollection("postsByYear", (collection) => {
+  return _.chain(collection.getAllSorted())
+    .groupBy((post) => post.date.getFullYear())
+    .toPairs()
+    .reverse()
+    .value();
 });
   
   
